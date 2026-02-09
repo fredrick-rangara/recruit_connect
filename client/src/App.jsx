923f1265365d064009bb15f8da646a30421593c1
fredrck rangara <ybnfredae@gmail.com>
2026-02-10 02:42:51 +0300

    edit App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard'; // Ensure this exists
+import JobBoard from './pages/JobBoard';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
             <Route path="/" element={<h1 className="text-center text-2xl font-bold">Home</h1>} />
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/jobs" element={<JobBoard />} />
           </Routes>
         </main>
       </div>
