645ef0f59e4d97e796c87647a6c38a056884c1dd
fredrck rangara <ybnfredae@gmail.com>
2026-02-10 10:26:42 +0300

    final commit

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-import sys
-import os
-
-# Force Python to see the 'server' directory
-sys.path.append(os.path.join(os.path.dirname(__file__), 'server'))
-
-from server import create_app, db
-from flask_migrate import Migrate
+from server import create_app
 
 app = create_app()
-migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
