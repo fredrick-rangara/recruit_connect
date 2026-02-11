0db40e3f5eea0aec03a4d1034b59ab4c5dc42864
abigaelseenoi <abigaelseenoi5@gmail.com>
2026-02-12 01:23:08 +0300

    final commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // This ensures your env variables are available
+    'process.env': {}
+  }
 })
